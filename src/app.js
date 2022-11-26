4bbb7974b71ceee8b693860f2e2815365d3cf698
lakhichan <lakhichan.roy007@gmail.com>
2022-11-26 13:58:48 +0530

    added more file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express =require("express")
 const app =express()
-
+const qrcode = require("qrcode")
 app.set("view engine" , "ejs")
 app.use(express.json())
+app.use(express.urlencoded({extended:false}));
 
 
 app.get("/",(req,res)=>{
     res.render("qrcode.ejs")
 })
 
+app.post("/qrcode",(req,res)=>{
+
+const mytext= req.body.url
+
+    qrcode.toDataURL(mytext,(err,code)=>{
+        res.render("codeImage.ejs",{
+            myqr:code
+        })
+    })
+})
 
 
 app.listen(5005,()=>{
